68f78bca9c0cabd52795a9985369a5cefde16e50
yazan.codavia <yazankassam.codavia@gmail.com>
2021-08-10 12:11:22 +0300

    add avatar property

--- DataAccess/Data/Entity/ApplicationUser.cs
+++ DataAccess/Data/Entity/ApplicationUser.cs
@@ -1,3 +1,4 @@
+using System;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using iread_identity_ms.DataAccess.Data.Type;
@@ -5,17 +6,19 @@ using Microsoft.AspNetCore.Identity;
 
 namespace iread_identity_ms.DataAccess.Data.Entity
 {
-    public class ApplicationUser : IdentityUser{
+    public class ApplicationUser : IdentityUser
+    {
 
 
         [Required(AllowEmptyStrings = true)]
         public string Password { get; set; }
-        
+
         [Required]
         [EnumDataType(typeof(RoleTypes), ErrorMessage = "Role type value doesn't exist within enum should be one of [Administrator, Teacher, Student]")]
         public string Role { get; set; }
 
         [Required(AllowEmptyStrings = false)]
         public string Name { get; set; }
+        public Nullable<int> Avatar { get; set; }
     }
 }
---
+++ Migrations/IdentityServer/ApplicationDb/20210810090922_avatar.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+namespace iread_identity_ms.Migrations.IdentityServer.ApplicationDb
+{
+    public partial class avatar : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "StoredSalt",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "UserId",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Avatar",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Avatar",
+                table: "AspNetUsers");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "StoredSalt",
+                table: "AspNetUsers",
+                type: "varbinary(128)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: false,
+                defaultValue: 0)
+                .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn);
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    Email = table.Column<string>(type: "text", nullable: false),
+                    Name = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    Role = table.Column<string>(type: "text", nullable: false),
+                    StoredSalt = table.Column<byte[]>(type: "varbinary(128)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.UserId);
+                });
+        }
+    }
+}
