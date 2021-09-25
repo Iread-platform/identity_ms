57d71f806ec5a7b213532bc080f202b3df62f3fb
codavia_kinda <codavia.kinda.flihan@gmail.com>
2021-09-25 16:40:26 +0300

    IR-232 Edit get school member result.

---
+++ Web/Dto/ClassDto/InnerClassDto.cs
@@ -0,0 +1,9 @@
+﻿namespace iread_identity_ms.Web.Dto.ClassDto
+{
+    public class InnerClassDto
+    {
+        public int ClassId { get; set; }
+        public string Title { get; set; }
+        public bool Archived { get; set; }
+    }
+}
--- Web/Dto/SchoolMemberDto/InnerSchoolMemberDto.cs
+++ Web/Dto/SchoolMemberDto/InnerSchoolMemberDto.cs
@@ -1,7 +1,12 @@
-﻿namespace iread_identity_ms.Web.Dto.SchoolMemberDto
+﻿using System.Collections.Generic;
+using iread_identity_ms.Web.Dto.ClassDto;
+
+namespace iread_identity_ms.Web.Dto.SchoolMemberDto
 {
     public class InnerSchoolMemberDto
     {
-         public int SchoolId { get; set; }
+        public int SchoolId { get; set; }
+        public string SchoolTitle { get; set; }
+        public IEnumerable<InnerClassDto> Classes { get; set; }
     }
 }
